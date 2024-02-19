04c68d810f952b6dc0d96c01ebf02b2950280683
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-02-19 14:56:08 +0530

    custom 404

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import { notFound } from "next/navigation"
+
+export const dynamincParams=true
+
+
+export async function generateStaticParams(){
+    const res = await fetch('http://localhost:4000/tickets')
+
+    const tickets=await res.json()
+    return tickets.map((ticket)=>({
+        id: ticket.id
+    }))
+}
+
+
 async function getTicket(id){
     const res=await fetch('http://localhost:4000/tickets/'+id,{
         next:{
             revalidate: 60
         }
     })
+
+    if(!res.ok){
+        notFound()
+    }
+
 
     return res.json()
 }
